ae04147b617af78abac3fafbfb0ce6b8bcb04b0b
wogml3270 <78224412+wogml3270@users.noreply.github.com>
2021-02-19 19:17:42 +0900

    version1.0

---
+++ my-portfolio/script/script.js
@@ -0,0 +1,73 @@
+var scroll = function(){
+    
+    var $nav = null,
+        $cnt = null,
+        moveCnt = null,
+        moveIndex = 0,
+        moveCntTop = 0,
+        winH = 0,
+        time = false;
+
+    $(document).ready(function(){
+        init();
+        initEvent();
+    });
+    
+    var init = function(){
+        $cnt = $(".content");
+        $nav = $(".header a");
+    };
+    
+    var initEvent = function(){
+        winResize();
+        $("html ,body").scrollTop(0);
+        $(window).resize(function(){
+            winResize();
+        });
+        $nav.on("click", function(){
+            moveIndex = $(this).parent("li").index();
+            moving(moveIndex);
+            return false;
+        });
+        $cnt.on("mousewheel", function(e){
+            if(time === false){ // time 변수가 false일때만 wheel 이벤트 실행
+              wheel(e);
+            }
+        });
+    };
+        
+    var winResize = function(){
+        winH = $(window).height();
+        $cnt.children("div").height(winH);
+        $("html ,body").scrollTop(moveIndex.scrollTop);
+    };
+    
+    var wheel = function(e){
+        if(e.originalEvent.wheelDelta < 0){
+            if(moveIndex < 4){
+                moveIndex += 1;
+                moving(moveIndex);
+            };
+        }else{
+            if(moveIndex > 0){
+                moveIndex -= 1;
+                moving(moveIndex);
+            };
+        };
+    };
+    
+    var moving = function(index){
+        time = true // wheel 이벤트 실행 동시에 true로 변경
+        moveCnt = $cnt.children("div").eq(index);
+        moveCntTop = moveCnt.offset().top;
+        $("html ,body").stop().animate({
+            scrollTop: moveCntTop
+        }, 300, function(){
+          time = false; // wheel 이벤트가 끝나면 false로 변경
+        });
+        $nav.parent("li").eq(index).addClass("on").siblings().removeClass("on");
+    };
+    
+};
+
+scroll();
